bc494b8cdfdedd4fdc2d13c17bd657374ab61322
daniquinto <dany.quintorios@gmail.com>
2026-04-26 15:17:44 -0500

    feat: add gh-pages deployment configuration and script dependencies

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/Mystore/", // Repository name for GitHub Pages deployment
 })
